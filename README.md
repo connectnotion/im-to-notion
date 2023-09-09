eb8183012e0ee9ce4d9b71a63966a94d1409dd59
Ashing Zheng <axingfly@gmail.com>
2023-09-09 21:23:17 +0800

    feat: support env (#19)
    Signed-off-by: ashing <axingfly@gmail.com>

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 - [im-to-notion](#im-to-notion)
   - [set config file](#set-config-file)
-  - [build image on local](#build-image-on-local)
+  - [build on local](#build-on-local)
   - [use release image](#use-release-image)
 - [Acknowledgement](#acknowledgement)
 - [Buy me a coffee](#buy-me-a-coffee)
@@ -33,20 +33,36 @@ destination:
     databaseId: xxx
 ```
 
-## build image on local
+## build on local
 
 ```shell
-make build-docker-image
+make build
 ```
 
 ```shell
-docker run -itd -v `pwd`/conf:/usr/local/im-to-notion/conf --name im-to-notion im-to-notion:dev -c /usr/local/im-to-notion/conf/config.yaml
+# after modify ./conf/config.yaml
+./bin/im-to-notion
 ```
 
 ## use release image
 
+- use env
+
 ```shell
-docker run -itd -v `pwd`/conf:/usr/local/im-to-notion/conf --name im-to-notion docker pull ghcr.io/ronething/im-to-notion:0.0.1 -c /usr/local/im-to-notion/conf/config.yaml
+docker run -itd \
+ -e LOG_LEVEL=${LOG_LEVEL} \
+ -e LOG_OUTPUT=${LOG_OUTPUT} \
+ -e DINGTALK_APP_KEY=${DINGTALK_APP_KEY} \
+ -e DINGTALK_APP_SECRET=${DINGTALK_APP_SECRET} \
+ -e NOTION_SECRET=${NOTION_SECRET} \
+ -e NOTION_DATABASE_ID=${NOTION_DATABASE_ID} \
+ --name im-to-notion ghcr.io/ronething/im-to-notion:0.0.1
+```
+
+- use config file
+
+```shell
+docker run -itd -v `pwd`/conf:/usr/local/im-to-notion/conf --name im-to-notion ghcr.io/ronething/im-to-notion:0.0.1 -c /usr/local/im-to-notion/conf/config.yaml
 ```
 
 # Acknowledgement
--- internal/pkg/config/default.go
+++ internal/pkg/config/default.go
@@ -1,14 +1,70 @@
 package config
 
-import "github.com/spf13/viper"
+import (
+	"os"
 
+	"github.com/spf13/viper"
+)
+
+// ViperDefaultValue set default config
 func ViperDefaultValue() {
-
 	viper.SetDefault("log.level", "info")
 	viper.SetDefault("log.output", "stderr")
+}
 
-	viper.SetDefault("source.dingtalk.appKey", "")
-	viper.SetDefault("source.dingtalk.appSecret", "")
+// SetValueFromEnv set config from env
+func SetValueFromEnv() error {
+	var (
+		logOptions  LogOptions
+		source      Source
+		destination Destination
+	)
 
-	viper.SetDefault("destination.notion.secret", "")
+	if err := viper.UnmarshalKey("log", &logOptions); err != nil {
+		return err
+	}
+	if err := viper.UnmarshalKey("source", &source); err != nil {
+		return err
+	}
+	if err := viper.UnmarshalKey("destination", &destination); err != nil {
+		return err
+	}
+
+	logLevel := os.Getenv("LOG_LEVEL")
+	if len(logLevel) > 0 {
+		logOptions.Level = logLevel
+	}
+	logOutput := os.Getenv("LOG_OUTPUT")
+	if len(logOutput) > 0 {
+		logOptions.Output = logOutput
+	}
+	viper.Set("log", logOptions)
+
+	dingtalkAppKey := os.Getenv("DINGTALK_APP_KEY")
+	if len(dingtalkAppKey) > 0 {
+		if source.Dingtalk == nil {
+			source.Dingtalk = &Dingtalk{
+				AppKey: dingtalkAppKey,
+			}
+		} else {
+			source.Dingtalk.AppKey = dingtalkAppKey
+		}
+	}
+	dingtalkAppSecret := os.Getenv("DINGTALK_APP_SECRET")
+	if len(dingtalkAppSecret) > 0 {
+		source.Dingtalk.AppSecret = dingtalkAppSecret
+	}
+	viper.Set("source", source)
+
+	notionSecret := os.Getenv("NOTION_SECRET")
+	if len(notionSecret) > 0 {
+		destination.Notion.Secret = notionSecret
+	}
+	notionDatabaseId := os.Getenv("NOTION_DATABASE_ID")
+	if len(notionDatabaseId) > 0 {
+		destination.Notion.DatabaseId = notionDatabaseId
+	}
+	viper.Set("destination", destination)
+
+	return nil
 }
